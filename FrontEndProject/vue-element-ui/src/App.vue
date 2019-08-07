c3d9bea32d3e0d23d53178abf22ffffa4aafb8af
xielbs <928135874@qq.com>
2019-08-07 10:36:51 +0800

    init

--- FrontEndProject/vue-element-ui/src/App.vue
+++ FrontEndProject/vue-element-ui/src/App.vue
@@ -1,8 +1,10 @@
 <template>
+
   <div id="app">
     <!--<img src="./assets/logo.png">-->
     <!--这里的router-link 就是将组件渲染到这里，实际是一个a标签，to写的是path路径，和index.js中是一致的。-->
-    <router-link to="/duanzi">duanzi页面</router-link>
+    <!--<router-link to="/duanzi">duanzi页面</router-link>-->
+    <!--<router-link to="/duanzi">duanzi页面</router-link>-->
     <router-view/>
   </div>
 </template>
@@ -14,12 +16,21 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html{
+    height: 100%;
+  }
+  body{
+    height: 100%;
+    margin: 0px;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    height: 100%;
+    /*margin-top: 60px;*/
+  }
+
 </style>
